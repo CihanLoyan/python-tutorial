1166f139e60218fe4ad67608c71672d94883bb6a
cloyan <cloyan@ebay.com>
2022-06-03 15:22:35 +0300

    Added sample for loop

--- 10.donguler.py
+++ 10.donguler.py
@@ -1,125 +1,165 @@
 
-toplam = 0
-while toplam <= 5:
-    print(f"toplam: {toplam}")
-    toplam += 1
-else:
-    print("donguden çıkıldı")
+# toplam = 0
+# while toplam <= 5:
+#     print(f"toplam: {toplam}")
+#     toplam += 1
+# else:
+#     print("donguden çıkıldı")
 
 
-print("***********************************************************************************")
+# print("***********************************************************************************")
 
 
-sayi = int(input('Faktöriyeli hesaplanacak sayıyı giriniz: '))
-fakt = 1
-while sayi > 0:
-    fakt = fakt * sayi
-    sayi -= 1 
-print('Sonuç: ', fakt)
+# sayi = int(input('Faktöriyeli hesaplanacak sayıyı giriniz: '))
+# fakt = 1
+# while sayi > 0:
+#     fakt = fakt * sayi
+#     sayi -= 1 
+# print('Sonuç: ', fakt)
 
 
-print("***********************************************************************************")
+# print("***********************************************************************************")
 
-devamMi = 'e'
-while devamMi == 'e':
-    sayi = int(input('Sayiyi giriniz: '))
-    asalMi = True
-    if sayi >= 2:
-        for i in range(2, sayi):
-            if sayi % i == 0:
-                asalMi = False
-                break 
-        print('asal değildir') if asalMi == False else print('asaldır')
-        devamMi = input('Devam etmek istiyor musunuz?(e/h)')
+# devamMi = 'e'
+# while devamMi == 'e':
+#     sayi = int(input('Sayiyi giriniz: '))
+#     asalMi = True
+#     if sayi >= 2:
+#         for i in range(2, sayi):
+#             if sayi % i == 0:
+#                 asalMi = False
+#                 break 
+#         print('asal değildir') if asalMi == False else print('asaldır')
+#         devamMi = input('Devam etmek istiyor musunuz?(e/h)')
 
 
-print("***********************************************************************************")
+# print("***********************************************************************************")
 
-for harf in 'Bursa':
-    print("Geçerli harf:", harf)
+# for harf in 'Bursa':
+#     print("Geçerli harf:", harf)
     
-print("**********************************************************************************")
+# print("**********************************************************************************")
 
-for harf in 'Bursa':
-    print(f"Geçerli harf: {harf}")
+# for harf in 'Bursa':
+#     print(f"Geçerli harf: {harf}")
 
-print("**********************************************************************************")
+# print("**********************************************************************************")
 
-meyveler = ["muz", "çilek", "ananas", "kiraz", "erik", "şeftali", "armut", "ayva"]
-for meyve in meyveler:
-    print("Meyve:", meyve)
+# meyveler = ["muz", "çilek", "ananas", "kiraz", "erik", "şeftali", "armut", "ayva"]
+# for meyve in meyveler:
+#     print("Meyve:", meyve)
 
-print("***********************************************************************************")
+# print("***********************************************************************************")
 
-# Tek parametre verirsek start 0, stop verdiğimiz parametre değerini alır. Step ise otomatik olarak 1 olur.
-for i in range(2):  # for(i=0; i<2; i++)
-    print("Geçerli meyve: ", meyveler[i])   
+# # Tek parametre verirsek start 0, stop verdiğimiz parametre değerini alır. Step ise otomatik olarak 1 olur.
+# for i in range(2):  # for(i=0; i<2; i++)
+#     print("Geçerli meyve: ", meyveler[i])   
 
-print("***********************************************************************************")
+# print("***********************************************************************************")
 
-# İki parametreli olursa start 1. parametre, stop 2. parametre, step ise 1 olarak ayarlanır.
-for m in range(2, 6):   # for(i=2; i<6; i++)
-    print("Geçerli meyve: ", meyveler[m])
+# # İki parametreli olursa start 1. parametre, stop 2. parametre, step ise 1 olarak ayarlanır.
+# for m in range(2, 6):   # for(i=2; i<6; i++)
+#     print("Geçerli meyve: ", meyveler[m])
 
-print("***********************************************************************************")
+# print("***********************************************************************************")
     
-# Üç parametreli olursa 3. parametre step değerini alır.
-for k in range(3, 7, 2):    # for(i=3; i<7; i+=2)
-    print(f"{k}. meyve: ", meyveler[k])
+# # Üç parametreli olursa 3. parametre step değerini alır.
+# for k in range(3, 7, 2):    # for(i=3; i<7; i+=2)
+#     print(f"{k}. meyve: ", meyveler[k])
 
-print("***********************************************************************************")
+# print("***********************************************************************************")
 
-# Tek parametre almakla aynıdır. start 0, stop verilen parametre, step 1 olarak ayarlanır.
-for t in range(len(meyveler)):
-    # print(f"{t}. Meyve:", meyveler[t])
-    print(str((t+1)) + ".", "Meyve:", meyveler[t])
+# # Tek parametre almakla aynıdır. start 0, stop verilen parametre, step 1 olarak ayarlanır.
+# for t in range(len(meyveler)):
+#     # print(f"{t}. Meyve:", meyveler[t])
+#     print(str((t+1)) + ".", "Meyve:", meyveler[t])
 
-print("***********************************************************************************")
+# print("***********************************************************************************")
 
-liste = [[1, 2], [3, 4]]
-for x in liste:
-    print(x)
-
-
-print("***********************************************************************************")
+# liste = [[1, 2], [3, 4]]
+# for x in liste:
+#     print(x)
 
 
-liste1 = [[1, 2],[3, 4]]
-for k, t in liste1:
-    print(f'Liste1:  k:{k} t:{t}')
+# print("***********************************************************************************")
 
 
-# liste2 = [[1, 2], [3, 4], [5, 6]]
-# for x, y, z in liste2:
-#     print(x, y, z)    # ÇALIŞIR MI?
+# liste1 = [[1, 2],[3, 4]]
+# for k, t in liste1:
+#     print(f'Liste1:  k:{k} t:{t}')
 
 
-liste3 = [[1, 2, 3], [4, 5, 6]]
-for x, y, z in liste3:
-     print(f'Liste3:  x:{x} y:{y} z:{z}')
+# # liste2 = [[1, 2], [3, 4], [5, 6]]
+# # for x, y, z in liste2:
+# #     print(x, y, z)    # ÇALIŞIR MI?
 
 
-print("***********************************************************************************")
+# liste3 = [[1, 2, 3], [4, 5, 6]]
+# for x, y, z in liste3:
+#      print(f'Liste3:  x:{x} y:{y} z:{z}')
 
 
-bilgi = {
-    'ad': 'cihan',
-    'yas': 24
-}
-print('Bilgi:', bilgi.items())
+# print("***********************************************************************************")
 
-print("***********************************************************************************")
 
-for b in bilgi.items():
-    print(b)
+# bilgi = {
+#     'ad': 'cihan',
+#     'yas': 24
+# }
+# print('Bilgi:', bilgi.items())
 
-print("***********************************************************************************")
+# print("***********************************************************************************")
 
-print('Keys:', bilgi.keys())
-print('Values:', bilgi.values())
+# for b in bilgi.items():
+#     print(b)
 
-print("***********************************************************************************")
+# print("***********************************************************************************")
 
-for k, v in bilgi.items():
-    print(f'Key: {k} \t Value: {v}')
+# print('Keys:', bilgi.keys())
+# print('Values:', bilgi.values())
 
+# print("***********************************************************************************")
+
+# for k, v in bilgi.items():
+#     print(f'Key: {k} \t Value: {v}')
+
+
+# print("***********************************************************************************")
+
+
+# rangeDizi = range(10)
+# print(rangeDizi, type(rangeDizi))
+
+# listeDonusum = list(range(10)) # Type Casting  
+# print(listeDonusum, type(listeDonusum))
+# print(list(range(2, 13)))
+# print(list(range(2, 13, 3)))
+
+# listeDonusum2 = [*range(10)] # Yukarıdaki yazıma alternatif düşünülebilir fakat yukarıdaki yazım tercih edilir.
+# print(listeDonusum2, type(listeDonusum2))
+
+# print([*range(3, 9, 2)])
+
+# print("***********************************************************************************")
+
+# harflerDizisi = ['a', 'b', 'c', 'd']
+# for i in enumerate(harflerDizisi):
+#     print(i)
+
+
+# harflerDizisi2 = ['a', 'b', 'c', 'd']
+# for index, harf in enumerate(harflerDizisi):
+#     print(f'Index: {index} \t Harf: {harf}')
+
+
+# harflerDizisi3 = ['a', 'b', 'c', 'd']
+# for index, harf in enumerate(harflerDizisi):
+#   print(f'{index + 1}. Harf: {harf}')
+
+
+ulkeKısaltma = ['TR', 'DE', 'FR']
+ulkeIsim = ['TÜRKİYE', 'almanya', 'fransa']
+siralama = range(1, 4)
+
+for i in zip(siralama, ulkeKısaltma, ulkeIsim):
+    print(i)
