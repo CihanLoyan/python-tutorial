c479a63e904f33de8314394690cbc13334614a36
cloyan <cloyan@ebay.com>
2022-06-17 23:05:56 +0300

    Add second try except sample

--- 16_try_except.py
+++ 16_try_except.py
@@ -1,19 +1,38 @@
-from time import sleep
+# from time import sleep
 
-def bolme_islemi():
-    sayi1 = int(input('Birinci sayiyi giriniz:'))
-    while True:
-        sayi2 = int(input('İkinci sayiyi giriniz:'))
-        print('Hesaplanıyor...')
-        sleep(2)
+def tam_sayiya_yuvarla():
+    girdi = input('Ondalık sayı giriniz: ')
+    
+    status = False
 
-        try:
-            sonuc = sayi1 / sayi2
-        except:
-            print("0'a bölünme hatası!")
-            continue
-        break
+    try:
+        girdi = float(girdi)
+        print(f'Yuvarlama işleminin sonucu: {round(girdi)}')
+        status = True
+    except:
+        print(f'Girilen {girdi} sayısı ondalık değil!')
+        status = False
+    finally:
+        print(f'Tam sayiya çevirme işleminin sonucu: {status}')
 
-    print(f'Sonuç: {sonuc}')
+tam_sayiya_yuvarla()
 
-bolme_islemi()
+print('------------------- İkinci Örnek --------------------')
+
+# def bolme_islemi():
+#     sayi1 = int(input('Birinci sayiyi giriniz:'))
+#     while True:
+#         sayi2 = int(input('İkinci sayiyi giriniz:'))
+#         print('Hesaplanıyor...')
+#         sleep(2)
+
+#         try:
+#             sonuc = sayi1 / sayi2
+#         except:
+#             print("0'a bölünme hatası!")
+#             continue
+#         break
+
+#     print(f'Sonuç: {sonuc}')
+
+# bolme_islemi()
